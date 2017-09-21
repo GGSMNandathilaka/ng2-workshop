dc40b7b3bbafec647aa150f83766cc2c548661b3
shehann <shehann@codegen.net>
2017-09-21 18:00:14 +0530

    Added room selection

--- src/app/room-card/room-card.component.ts
+++ src/app/room-card/room-card.component.ts
@@ -9,6 +9,7 @@ import {Room} from '../../models/room';
 export class RoomCardComponent implements OnInit {
 
   @Input() room: Room;
+  roomCount = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10'];
 
   constructor() {
   }
